a414803d3e2872aadb9ea492dfda6e8b8091ed71
CarlosMoraya <cadu.moraya@gmail.com>
2026-01-29 20:22:40 -0300

    Fix case sensitivity in settings for deployment

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = '.env'
-        case_sensitive = True
+        case_sensitive = False
 
 @lru_cache
 def get_settings() -> 'Settings':
